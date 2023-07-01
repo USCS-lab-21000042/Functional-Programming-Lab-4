b45f657ebc81641a0d314e9835a346fa994d03b0
visal <visaluni2@gmail.com>
2023-07-01 15:57:53 +0530

    define function by values

--- Q3.scala
+++ Q3.scala
@@ -1,10 +1,14 @@
 object test extends App{
-    def toUpper(x:String):String={
-        x.toUpperCase()
-    }
-    def toLower(x:String):String={
-        x.toLowerCase()
-    }
+    // def toUpper(x:String):String={
+    //     x.toUpperCase()
+    // }
+    // def toLower(x:String):String={
+    //     x.toLowerCase()
+    // }
+
+    var toLower=(x:String)=>x.toLowerCase()
+    val toUpper=(x:String)=>x.toUpperCase()
+
     var list=List("kamaL","Asiri","jayantha")
     //println(toLower("TREDSDSD"))
     var out=list.map(list=>toLower(list))
