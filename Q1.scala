7cc10881d7f5098eef91fca9f4ac3b3d225aca94
visal <visaluni2@gmail.com>
2023-07-03 10:34:28 +0530

    defined function using by values

--- Q1.scala
+++ Q1.scala
@@ -7,10 +7,10 @@ object test extends App{
     // }
 
     val interst=(x:Double) =>x match{
-        case x if x<=20000 => x*2/100
-        case x if x>20000 && x<=200000 => x*4/100.0
-        case x if x>200000 && x<=2000000 =>x*3.5/100.0
-        case x if x>2000000 => x*6.5/100.0
+        case x if (x<=20000    =)> x*2/100
+        case x if (x>20000) && (x<=200000)    => x*4/100.0
+        case x if (x>200000) && (x<=2000000)  =>x*3.5/100.0
+        case x if (x>2000000)  => x*6.5/100.0
     }
 
     //println("Interest :"+interst(252))
